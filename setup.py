c8267b48f45410ff525ea2a8a18fc7904a62339f
“Andrew <xhinker@hotmail.com>
2024-12-09 10:12:27 -0800

    add code for lama inpaint

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup,find_packages
 
 setup(
     name               = 'azailib'
-    , version          = '1.241208.1'
+    , version          = '1.241209.1'
     , license          = 'Apache License'
     , author           = "Andrew Zhu"
     , author_email     = 'xhinker@hotmail.com'
